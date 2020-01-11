2f8efd97930cee8fce4f83b2ec374168cb5ba728
unknown <jcalderon.aprog@gmail.com>
2020-01-11 05:11:35 -0300

    trascender

--- app/backend/script/new.user.js
+++ app/backend/script/new.user.js
@@ -4,30 +4,29 @@ const rl = readline.createInterface({
 	output: process.stdout
 });
 
-rl.question('database: ', (database) => {
-	rl.question('email: ', (email) => {
-		rl.question('password: ', (password) => {
-			rl.question('roles: ', (roles) => {
-				let helper	= new (require("../lib/helper"))();
-				let mongodb	= new (require("../lib/mongodb"))();
-				let create = async function(database,email,password,roles){
-					let doc = {};
-					doc.email = email;
-					doc.hash = helper.random(10);
-					doc.password = helper.toHash(password + email,doc.hash);
-					doc.nickname = email;
-					doc.notification = true;
-					doc.thumb = "/media/img/user.png";
-					doc.roles = roles.split(",");
-					doc.created = new Date;
-					doc.activate = true;
-					
-					let db = await mongodb.connect("mongodb://127.0.0.1:27017/" + database);
-					await mongodb.insertOne(db,"user",doc,true);
-					console.log("usuario administrador configurado correctamente");
-				}
-				create(database,email,roles);
-			});
+rl.question('email: ', (email) => {
+	rl.question('password: ', (password) => {
+		rl.question('roles: ', (roles) => {
+			const fs = require("fs");
+			const helper = new (require("../lib/helper"))();
+			const mongodb = new (require("../lib/mongodb"))();
+			const config = JSON.parse(fs.readFileSync("../../../app.json","utf-8"));
+			let create = async function(email,password,roles){
+				let doc = {};
+				doc.email = email;
+				doc.hash = helper.random(10);
+				doc.password = helper.toHash(password + email,doc.hash);
+				doc.nickname = email;
+				doc.notification = true;
+				doc.thumb = "/media/img/user.png";
+				doc.roles = roles.split(",");
+				doc.created = new Date;
+				doc.activate = true;
+				let db = await mongodb.connect(config.database.url);
+				await mongodb.insertOne(db,"user",doc,true);
+				console.log("usuario administrador configurado correctamente");
+			}
+			create(email,password,roles);
 		});
 	});
 });
