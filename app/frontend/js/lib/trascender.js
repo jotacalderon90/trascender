51cab12ce50e58a639faeecb304396b0b814877c
unknown <jcalderon.aprog@gmail.com>
2020-01-22 23:23:56 -0300

    trascender

--- app/frontend/js/lib/trascender.js
+++ app/frontend/js/lib/trascender.js
@@ -578,21 +578,12 @@ trascender.prototype.SOABF = function(coll,field){
 
 //GENERIC
 trascender.prototype.cleaner = function(cadena){
-	// Definimos los caracteres que queremos eliminar
 	var specialChars = "!@#$^&%*()+=-[]\/{}|:<>?,.`";
-	
-	// Los eliminamos todos
 	for (var i = 0; i < specialChars.length; i++) {
 		cadena= cadena.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
 	}
-	
-	// Lo queremos devolver limpio en minusculas
 	cadena = cadena.toLowerCase();
-	
-	// Quitamos espacios y los sustituimos por _ porque nos gusta mas asi
 	cadena = cadena.replace(/ /g,"-");
-	
-	// Quitamos acentos y "ñ". Fijate en que va sin comillas el primer parametro
 	cadena = cadena.replace(/á/gi,"a");
 	cadena = cadena.replace(/é/gi,"e");
 	cadena = cadena.replace(/í/gi,"i");
