c29ad58d0afc1b897b43e37b9d3336ee0bf8391b
unknown <jcalderon.aprog@gmail.com>
2020-01-26 23:31:43 -0300

    trascender

--- app/backend/lib/log.js
+++ app/backend/lib/log.js
@@ -11,38 +11,38 @@ let self = function(a){
 //registro de logs que entran sin ser identificados
 self.prototype.create = async function(req){
 	try{
-		//si cambio la fecha, entonces limpiar cache
-		if(req.dateref.year!=this.dateref.year || req.dateref.month!=this.dateref.month || req.dateref.day!=this.dateref.day){
-			let d = new Date();
-			this.dateref = { year: d.getFullYear(), month: d.getMonth(), day: d.getDate()};
-			this.ips = [];
-		}
-		
-		//ingresa por primera vez en el día
-		if(this.ips.indexOf(req.real_ip)==-1){
-			console.log(req.user);
-			let db = await this.mongodb.connect(this.config.database.url);
-			if(req.user){
-				//nueva ip de usuario authenticado
-				user = await this.mongodb.findOne(db,"user",req.user.sub);
-				user.ip = (user.ip)?user.ip:[];
-				if(user.ip.indexOf(req.real_ip)==-1){
-					user.ip.push(req.real_ip);
-				}
-				await this.mongodb.updateOne(db,"user",user._id,user);
-			}else{
-				let possibles = await this.mongodb.count(db,"user",{ip: {$in: [req.real_ip]}});
-				if(possibles==0){
-					await this.mongodb.insertOne(db,"log",{ip: req.real_ip, created: req.created, url: req.url, method: req.method, body: JSON.stringify(req.body)});
-				}
+		if(req.real_ip){
+			//si cambio la fecha, entonces limpiar cache
+			if(req.dateref.year!=this.dateref.year || req.dateref.month!=this.dateref.month || req.dateref.day!=this.dateref.day){
+				let d = new Date();
+				this.dateref = { year: d.getFullYear(), month: d.getMonth(), day: d.getDate()};
+				this.ips = [];
 			}
-			let docip = await this.mongodb.find(db,"ip",{ip: req.real_ip});
-			if(docip.length!=1){
-				console.log("inserta nueva ip: " + req.real_ip);
-				await this.mongodb.insertOne(db,"ip",{ip: req.real_ip, created: req.created, url: req.url, method: req.method, body: JSON.stringify(req.body)});
+			
+			//ingresa por primera vez en el día
+			if(this.ips.indexOf(req.real_ip)==-1){
+				let db = await this.mongodb.connect(this.config.database.url);
+				if(req.user){
+					//nueva ip de usuario authenticado
+					user = await this.mongodb.findOne(db,"user",req.user.sub);
+					user.ip = (user.ip)?user.ip:[];
+					if(user.ip.indexOf(req.real_ip)==-1){
+						user.ip.push(req.real_ip);
+					}
+					await this.mongodb.updateOne(db,"user",user._id,user);
+				}else{
+					let possibles = await this.mongodb.count(db,"user",{ip: {$in: [req.real_ip]}});
+					if(possibles==0){
+						await this.mongodb.insertOne(db,"log",{ip: req.real_ip, created: req.created, url: req.url, method: req.method, body: JSON.stringify(req.body)});
+					}
+				}
+				let docip = await this.mongodb.find(db,"ip",{ip: req.real_ip});
+				if(docip.length!=1){
+					await this.mongodb.insertOne(db,"ip",{ip: req.real_ip, created: req.created, url: req.url, method: req.method, body: JSON.stringify(req.body)});
+				}
+				this.ips.push(req.real_ip);
+				db.close();
 			}
-			this.ips.push(req.real_ip);
-			db.close();
 		}
 	}catch(e){
 		console.log(e);
